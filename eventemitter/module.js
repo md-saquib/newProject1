c1dd43c6666dac51b3fc864b10bf78d9d2d85c31
md-saquib <saquibmdansari01@gmail.com>
2024-08-06 21:26:00 +0530

    push

---
+++ eventemitter/module.js
@@ -0,0 +1,16 @@
+// console.log('hello');
+
+// const Event = require('events');
+// const Path = require('path');
+// const Os = require('os');
+// const Fs = require('fs');
+
+// // console.log(Event);
+
+// const eventEmitter = new Event();
+
+// // console.log(eventEmitter.addListener);
+// console.log(Os.hostname())
+// console.log(Fs);
+// // console.log(Path)
+console.log(global.performance);
